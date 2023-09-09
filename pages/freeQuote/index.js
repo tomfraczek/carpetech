f0f222623f8db482e1662b8c8c68a7d68e7f0b51
Tom Fraczek <tomfraczekdev@gmail.com>
2023-09-09 11:57:55 +0200

    working form

---
+++ pages/freeQuote/index.js
@@ -0,0 +1,87 @@
+'use client';
+import Head from 'next/head';
+import { useState } from 'react';
+
+export default function Quote() {
+  const [name, setName] = useState('');
+  const [email, setEmail] = useState('');
+  const [message, setMessage] = useState('');
+  const [submitted, setSubmitted] = useState(false);
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    console.log('Sending');
+
+    let data = {
+      name,
+      email,
+      message,
+    };
+
+    fetch('api/contact', {
+      method: 'POST',
+      headers: {
+        Accept: 'application/json, text/plain, */*',
+        'Content-Type': 'application/json',
+      },
+      body: JSON.stringify(data),
+    }).then((res) => {
+      console.log('Response received');
+      if (res.status === 200) {
+        console.log('Response succeeded!');
+        setSubmitted(true);
+        setName('');
+        setEmail('');
+        setMessage('');
+      }
+    });
+  };
+
+  return (
+    <main>
+      <div>
+        <form>
+          <div>
+            <label htmlFor='name'>Name</label>
+            <input
+              type='text'
+              onChange={(e) => {
+                setName(e.target.value);
+              }}
+              name='name'
+            />
+          </div>
+
+          <div>
+            <label htmlFor='email'>Email</label>
+            <input
+              type='email'
+              onChange={(e) => {
+                setEmail(e.target.value);
+              }}
+              name='email'
+            />
+          </div>
+
+          <div>
+            <label htmlFor='message'>Message</label>
+            <input
+              type='text'
+              onChange={(e) => {
+                setMessage(e.target.value);
+              }}
+              name='message'
+            />
+          </div>
+
+          <input
+            type='submit'
+            onClick={(e) => {
+              handleSubmit(e);
+            }}
+          />
+        </form>
+      </div>
+    </main>
+  );
+}
