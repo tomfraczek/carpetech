b5ae263be5aec07533cd467048ad85e4811678cc
Tom Fraczek <tomfraczekdev@gmail.com>
2023-10-22 18:25:33 +0200

    add testimonials page

---
+++ app/testimonials/constants.js
@@ -0,0 +1,6 @@
+import image1 from '@/public/images/about/image1.jpeg';
+
+export const CONTENT_BAR = {
+  title: 'Customer Reviews',
+  paragraph: 'Read Some Testimonials from Previous Satisfied Customers',
+};
--- app/testimonials/page.js
+++ app/testimonials/page.js
@@ -1,14 +1,12 @@
 'use client';
-import { ContentWithImage } from '../components/ContentWithImage';
 import { QuickQuote } from '../components/QuickQoute';
 import { TextBar } from '../components/TextBar';
-import { CONTENT_BAR, PAGE_SECTION_ONE } from './constants';
+import { CONTENT_BAR } from './constants';
 
 export default function Page() {
   return (
     <>
       <TextBar data={CONTENT_BAR} />
-      <ContentWithImage data={PAGE_SECTION_ONE} small />
       <QuickQuote />
     </>
   );
