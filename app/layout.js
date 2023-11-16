79f79599bf3b2adc7ae31e8c5f270a6a6a3011f9
Tom Fraczek <tomfraczekdev@gmail.com>
2023-11-16 15:43:25 +0100

    test

--- app/layout.js
+++ app/layout.js
@@ -19,20 +19,18 @@ const robotoCondensed = Roboto_Condensed({
 });
 
 export default async function RootLayout({ children }) {
-  const allPages = await getAllPages(false);
-  const menuItems = allPages.map(({ title, slug }) => ({
-    title,
-    slug,
-  }));
-
-  console.log(menuItems);
+  // const allPages = await getAllPages(false);
+  // const menuItems = allPages.map(({ title, slug }) => ({
+  //   title,
+  //   slug,
+  // }));
 
   return (
     <html lang='en'>
       <head></head>
       <body style={{ margin: 0 }} className={robotoCondensed.className}>
         <StyledComponentsRegistry>
-          <Header menu={menuItems} />
+          {/* <Header menu={menuItems} /> */}
           {children}
           <Footer />
         </StyledComponentsRegistry>
