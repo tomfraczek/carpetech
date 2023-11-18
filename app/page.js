8872410257f8b3eafc4922c67f664ab420bf4a3a
Tom Fraczek <tomfraczekdev@gmail.com>
2023-11-18 11:32:12 +0100

    test 1

--- app/page.js
+++ app/page.js
@@ -2,36 +2,34 @@
 import { draftMode } from 'next/headers';
 import { getHomepage } from '@/lib/api';
 import { Carousel } from '@/app/components/Carousel';
-import {
-  HOMEPAGE_CELEBRATION_SECTION,
-  HOMEPAGE_REPUTATION_SECTION,
-  HOMEPAGE_SLIDER_DATA,
-  SERVICES_DATA,
-} from '@/app/constants';
+import { HOMEPAGE_SLIDER_DATA, SERVICES_DATA } from '@/app/constants';
 
 import { ContentWithImage } from '@/app/components/ContentWithImage';
 import { ServicesCards } from '@/app/components/ServicesCards';
-import { CarpetCleaning } from '@/app/components/CarpetCleaning';
 import { ChemDryBar } from '@/app/components/ChemDryBar';
 import { QuickQuote } from '@/app/components/QuickQoute';
 
 export default async function Page() {
-  const { isEnabled } = draftMode();
-  const homepage = await getHomepage(isEnabled);
-  const { topSection, middleSection, bottomSection, fullWidthBar } = homepage;
-  return (
-    <>
-      {homepage && (
-        <>
-          <Carousel data={HOMEPAGE_SLIDER_DATA} />
-          <ContentWithImage {...topSection} />
-          <ServicesCards data={SERVICES_DATA} />
-          <ContentWithImage {...middleSection} />
-          <ChemDryBar {...fullWidthBar} />
-          <ContentWithImage {...bottomSection} />
-          <QuickQuote />
-        </>
-      )}
-    </>
-  );
+  try {
+    const { isEnabled } = draftMode();
+    const homepage = await getHomepage(isEnabled);
+    const { topSection, middleSection, bottomSection, fullWidthBar } = homepage;
+
+    return (
+      <>
+        <Carousel data={HOMEPAGE_SLIDER_DATA} />
+        <ContentWithImage {...topSection} />
+        <ServicesCards data={SERVICES_DATA} />
+        <ContentWithImage {...middleSection} />
+        <ChemDryBar {...fullWidthBar} />
+        <ContentWithImage {...bottomSection} />
+        <QuickQuote />
+      </>
+    );
+  } catch (error) {
+    console.error('Error fetching homepage data', error);
+
+    // Render a fallback UI or handle the error as needed
+    return <p>Error loading data.</p>;
+  }
 }
