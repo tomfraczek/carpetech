e62430b8043e4e0aec72de0f8e1679eb59e14d2e
Tom Fraczek <tomfraczekdev@gmail.com>
2023-11-18 11:50:59 +0100

    api requestes are back

--- app/layout.js
+++ app/layout.js
@@ -18,13 +18,19 @@ const robotoCondensed = Roboto_Condensed({
   preload: false,
 });
 
-export default function RootLayout({ children }) {
+export default async function RootLayout({ children }) {
+  const allPages = await getAllPages(false);
+  const menuItems = allPages.map(({ title, slug }) => ({
+    title,
+    slug,
+  }));
+
   return (
     <html lang='en'>
       <head></head>
       <body style={{ margin: 0 }} className={robotoCondensed.className}>
         <StyledComponentsRegistry>
-          {/* <Header menu={menuItems} /> */}
+          <Header menu={menuItems} />
           {children}
           <Footer />
         </StyledComponentsRegistry>
