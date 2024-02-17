dd11c8fcd070c5e26ca4d350492120b707c670d0
Tom Fraczek <tomfraczekdev@gmail.com>
2024-02-17 16:38:55 +0100

    add missing import

--- app/api/googleReviews/route.js
+++ app/api/googleReviews/route.js
@@ -1,3 +1,4 @@
+import axios from 'axios';
 const API_KEY = 'AIzaSyCemcFUJTtSY4pL6RaAL48N8uU3PT0pof8';
 const PLACE_ID = 'ChIJAfUrG0VEBEcRtEycalTUIso';
 export async function GET() {
@@ -5,6 +6,7 @@ export async function GET() {
     const response = await axios.get(
       `https://maps.googleapis.com/maps/api/place/details/json?place_id=${PLACE_ID}&fields=reviews&key=${API_KEY}`
     );
+    // console.log();
     return response;
   } catch (error) {
     console.error('Error fetching reviews:', error);
