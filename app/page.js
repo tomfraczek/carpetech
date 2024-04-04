627e21de5f7488046873726b804d4c6e259e2f95
unknown <tomfraczekdev@gmail.com>
2024-04-04 19:28:18 +0200

    feat: add redirect middleware

--- app/page.js
+++ app/page.js
@@ -1,13 +1,12 @@
-import { draftMode } from 'next/headers';
-import { getHomepage } from '@/lib/api';
-import { Carousel } from '@/app/components/Carousel';
+import { draftMode } from "next/headers";
+import { getHomepage } from "@/lib/api";
+import { Carousel } from "@/app/components/Carousel";
 
-import { ContentWithImage } from '@/app/components/ContentWithImage';
-import { ServicesCards } from '@/app/components/ServicesCards';
-import { QuickQuote } from '@/app/components/QuickQoute';
-import { GoogleMap } from '@/app/components/GoogleMap';
-import Script from 'next/script';
-import styled from 'styled-components';
+import { ContentWithImage } from "@/app/components/ContentWithImage";
+import { ServicesCards } from "@/app/components/ServicesCards";
+import { QuickQuote } from "@/app/components/QuickQoute";
+import { GoogleMap } from "@/app/components/GoogleMap";
+import Script from "next/script";
 
 export async function generateMetadata() {
   const { isEnabled } = draftMode();
@@ -23,7 +22,7 @@ export async function generateMetadata() {
       image: image.url,
     },
     verification: {
-      google: 'z5C3Tq0BEN9vFOUd8_9XUM5AmEZZKjB5CBiWIHTaQFo',
+      google: "z5C3Tq0BEN9vFOUd8_9XUM5AmEZZKjB5CBiWIHTaQFo",
     },
   };
 }
@@ -45,17 +44,22 @@ export default async function Page() {
 
   return (
     <>
-      <Script src='https://reviewsonmywebsite.com/js/v2/embed.js?id=7bf8acda5d5930b1a9db343a4ec1b31c' />
+      <Script src="https://reviewsonmywebsite.com/js/v2/embed.js?id=7bf8acda5d5930b1a9db343a4ec1b31c" />
       {heroSection && <Carousel {...heroSection} />}
       {topSection && <ContentWithImage {...topSection} />}
       {servicesCollection && <ServicesCards items={servicesCollection.items} />}
       {contactBar && <QuickQuote {...contactBar} />}
       {showReviews && (
-        <div className='reviewsContainer' data-romw-token='H2D7ArN2ny17e7RaGMsfyFYXI80Be3E2Wt5ZJDTu3PICCSfhMc' />
+        <div
+          className="reviewsContainer"
+          data-romw-token="H2D7ArN2ny17e7RaGMsfyFYXI80Be3E2Wt5ZJDTu3PICCSfhMc"
+        />
       )}
       {middleSection && <ContentWithImage {...middleSection} />}
       {bottomSection && <ContentWithImage {...bottomSection} />}
-      {bottomEmergencySection && <ContentWithImage {...bottomEmergencySection} />}
+      {bottomEmergencySection && (
+        <ContentWithImage {...bottomEmergencySection} />
+      )}
       {freeQuote && <QuickQuote {...freeQuote} />}
       <GoogleMap />
     </>
---
+++ middleware.ts
@@ -0,0 +1,21 @@
+// middleware.ts
+
+import { NextRequest, NextResponse } from "next/server";
+
+export async function middleware(request: NextRequest) {
+  const { host, protocol, pathname, search } = new URL(request.url);
+
+  // Check if the request is not HTTPS or not starting with "www."
+  if (protocol !== "https:" || !host.startsWith("www.")) {
+    // Redirect to the HTTPS and "www" version
+    const redirectTo = `https://www.${host.replace(
+      "www.",
+      ""
+    )}${pathname}${search}`;
+
+    return NextResponse.redirect(redirectTo, { status: 301 });
+  }
+
+  // Continue with the request
+  return NextResponse.next();
+}
