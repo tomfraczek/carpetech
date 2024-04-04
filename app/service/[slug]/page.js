c97593e88d2172a8508a59c4424ba1ecdd349f55
unknown <tomfraczekdev@gmail.com>
2024-04-04 20:19:16 +0200

    chore: remove unused pages, add canonical tag to the service page

--- app/service/[slug]/page.js
+++ app/service/[slug]/page.js
@@ -1,29 +1,31 @@
-// 'use client';
-import { draftMode } from 'next/headers';
-import { notFound } from 'next/navigation';
+import { draftMode } from "next/headers";
+import { notFound } from "next/navigation";
 
-import { getPage } from '@/lib/api';
+import { getPage } from "@/lib/api";
 
-import { Carousel } from '@/app/components/Carousel';
-import { ContentWithImage } from '@/app/components/ContentWithImage';
-import { ChemDryBar } from '@/app/components/ChemDryBar';
-import { QuickQuote } from '@/app/components/QuickQoute';
-import Script from 'next/script';
+import { Carousel } from "@/app/components/Carousel";
+import { ContentWithImage } from "@/app/components/ContentWithImage";
+import { QuickQuote } from "@/app/components/QuickQoute";
+import Script from "next/script";
 
 export async function generateMetadata({ params }) {
   const { isEnabled } = draftMode();
   const content = await getPage(params.slug, isEnabled);
+  const siteURL = "https://www.carpetech.co.uk";
   if (!content[0]) {
     notFound();
   }
   return {
-    title: content[0]?.seo?.title ?? '',
-    keywords: content[0]?.seo?.keywords ?? '',
-    description: content[0]?.seo?.description ?? '',
+    title: content[0]?.seo?.title ?? "",
+    keywords: content[0]?.seo?.keywords ?? "",
+    description: content[0]?.seo?.description ?? "",
     openGraph: {
-      title: content[0]?.seo?.title ?? '',
-      description: content[0]?.seo?.description ?? '',
-      image: content[0]?.seo?.image?.url ?? '',
+      title: content[0]?.seo?.title ?? "",
+      description: content[0]?.seo?.description ?? "",
+      image: content[0]?.seo?.image?.url ?? "",
+    },
+    alternates: {
+      canonical: `${siteURL}/${params.slug}`,
     },
   };
 }
@@ -46,16 +48,22 @@ export default async function Page({ params }) {
 
   return (
     <>
-      <Script src='https://reviewsonmywebsite.com/js/v2/embed.js?id=7bf8acda5d5930b1a9db343a4ec1b31c' />
+      <Script src="https://reviewsonmywebsite.com/js/v2/embed.js?id=7bf8acda5d5930b1a9db343a4ec1b31c" />
       {heroSection && <Carousel {...heroSection} />}
       {topSection && <ContentWithImage {...topSection} />}
       {topEmergencySection && <ContentWithImage {...topEmergencySection} />}
       {contactBar && <QuickQuote {...contactBar} />}
-      {showReviews && <div data-romw-token='H2D7ArN2ny17e7RaGMsfyFYXI80Be3E2Wt5ZJDTu3PICCSfhMc' />}
+      {showReviews && (
+        <div data-romw-token="H2D7ArN2ny17e7RaGMsfyFYXI80Be3E2Wt5ZJDTu3PICCSfhMc" />
+      )}
       {middleSection && <ContentWithImage {...middleSection} />}
-      {middleEmergencySection && <ContentWithImage {...middleEmergencySection} />}
+      {middleEmergencySection && (
+        <ContentWithImage {...middleEmergencySection} />
+      )}
       {bottomSection && <ContentWithImage {...bottomSection} />}
-      {bottomEmergencySection && <ContentWithImage {...bottomEmergencySection} />}
+      {bottomEmergencySection && (
+        <ContentWithImage {...bottomEmergencySection} />
+      )}
       {freeQuote && <QuickQuote {...freeQuote} />}
     </>
   );
